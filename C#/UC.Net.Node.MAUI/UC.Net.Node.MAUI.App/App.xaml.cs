f3e4cd7b7b22021eb2837d3a7339979d347ffaf5
mooshra <31731931+mooshra@users.noreply.github.com>
2022-07-08 20:26:34 +0300

    Projects Reorganization

---
+++ C#/UC.Net.Node.MAUI/UC.Net.Node.MAUI.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace UC.Net.Node.MAUI.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
