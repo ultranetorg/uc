39a27e721d877b5555cb69f679a41487e3282747
mooshra <31731931+mooshra@users.noreply.github.com>
2022-07-18 16:03:46 +0300

    Reorganize styling, coloring

--- C#/UC.Net.Node.MAUI/UC.Net.Node.MAUI/MauiProgram.cs
+++ C#/UC.Net.Node.MAUI/UC.Net.Node.MAUI/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace UC.Net.Node.MAUI;
+﻿using CommunityToolkit.Maui;
+
+namespace UC.Net.Node.MAUI;
 
 public static class MauiProgram
 {
@@ -7,6 +9,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
