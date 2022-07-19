1f821b0243b221d3d649ef2cd62d84d8809984eb
mooshra <31731931+mooshra@users.noreply.github.com>
2022-07-19 19:30:04 +0300

    Adapt controls folder

---
+++ C#/UC.Net.Node.MAUI/UC.Net.Node.MAUI/Controls/EmptyView.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace UC.Net.Node.MAUI.Controls
+{
+    public partial class EmptyView : ContentView
+    {
+        public EmptyView()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty TextProperty = BindableProperty.Create("Text", typeof(string), typeof(EmptyView), null);
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+    }
+}
