8a12a4695867fd25f917cd971d16ed908a676d48
mooshra <31731931+mooshra@users.noreply.github.com>
2022-12-12 20:32:55 +0200

    Account Popups Final Touches

--- C#/Umc/Views/Account/AccountView.xaml.cs
+++ C#/Umc/Views/Account/AccountView.xaml.cs
@@ -3,15 +3,24 @@
 public partial class AccountView : ContentView
 {
     public static readonly BindableProperty AccountProperty =
-		BindableProperty.Create(nameof(Account), typeof(AccountViewModel), typeof(BadgeView));
+		BindableProperty.Create(nameof(Account), typeof(AccountViewModel), typeof(AccountView));
 
-    public AccountViewModel Account
+	public static readonly BindableProperty BorderStyleProperty =
+		BindableProperty.Create(nameof(BorderStyle), typeof(Style), typeof(AccountView));
+
+	public AccountViewModel Account
     {
         get { return (AccountViewModel)GetValue(AccountProperty); }
         set { SetValue(AccountProperty, value); }
-    }
+	}
 
-    public AccountView()
+	public Style BorderStyle
+	{
+		get { return (Style)GetValue(BorderStyleProperty); }
+		set { SetValue(BorderStyleProperty, value); }
+	}
+
+	public AccountView()
     {
         InitializeComponent();
     }
