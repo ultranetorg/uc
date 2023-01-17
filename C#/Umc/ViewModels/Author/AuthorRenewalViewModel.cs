3363c5190416f17523361912427a184494bf15aa
mooshra <31731931+mooshra@users.noreply.github.com>
2023-01-18 00:39:56 +0200

    Author Renewal Page

--- C#/Umc/ViewModels/Author/AuthorRenewalViewModel.cs
+++ C#/Umc/ViewModels/Author/AuthorRenewalViewModel.cs
@@ -2,6 +2,14 @@
 
 public partial class AuthorRenewalViewModel : BaseAuthorViewModel
 {
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(EstimatedDate))]
+	private string _period = "1";
+
+	public string EstimatedDate => string.IsNullOrWhiteSpace(Period)
+		? string.Empty
+		: DateTime.UtcNow.AddDays(int.Parse(Period)).ToShortDateString();
+
     public AuthorRenewalViewModel(ILogger<AuthorRenewalViewModel> logger) : base(logger)
     {
     }
