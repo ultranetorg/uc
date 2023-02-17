19e80ade0afa9690ba13dfd614c8d8211be34dcd
mooshra <31731931+mooshra@users.noreply.github.com>
2023-02-17 21:12:35 +0200

    Delete Account Fix

--- C#/Umc/ViewModels/Popups/DeleteAccountPopupViewModel.cs
+++ C#/Umc/ViewModels/Popups/DeleteAccountPopupViewModel.cs
@@ -3,7 +3,13 @@
 public partial class DeleteAccountPopupViewModel : BaseViewModel
 {
 	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(TextToConfirm))]
 	public AccountViewModel _account;
+
+	public string TextToConfirm => Account?.Address[..9] ?? "0x07-47f0";
+
+	[ObservableProperty]
+	public string _textInput;
 
 	public DeleteAccountPopupViewModel(ILogger<DeleteAccountPopupViewModel> logger) : base(logger)
 	{
