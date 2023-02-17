80f8086d0e47ce0bc240f034e969ca48e02134b2
mooshra <31731931+mooshra@users.noreply.github.com>
2023-02-17 23:17:45 +0200

    ETHTransfer & Restore Account Pages (part 1)

---
+++ C#/Umc/Views/Controls/SelectPrivateKey.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Input;
+
+namespace UC.Umc.Controls;
+
+public partial class SelectPrivateKey : ContentView
+{
+    public static readonly BindableProperty SelectionChangedProperty =
+        BindableProperty.Create(nameof(SelectionChanged), typeof(ICommand), typeof(SelectPrivateKey), null,
+            BindingMode.TwoWay);
+
+	public static readonly BindableProperty IsPrivateKeyProperty =
+        BindableProperty.Create(nameof(IsPrivateKey), typeof(bool), typeof(SelectPrivateKey), true);
+
+    public ICommand SelectionChanged
+    {
+        get => (ICommand)GetValue(SelectionChangedProperty);
+        set => SetValue(SelectionChangedProperty, value);
+    }
+
+    public bool IsPrivateKey
+    {
+        get => (bool)GetValue(IsPrivateKeyProperty);
+        set => SetValue(IsPrivateKeyProperty, value);
+    }
+
+	public SelectPrivateKey()
+	{
+		InitializeComponent();
+	}
+}
