161314af48cc46d027752a86204bb9b3a5d761e4
Maximion <a@ultranet.org>
2023-03-14 21:08:01 +0200

    Revert "the work on dontnet deps loading"
    This reverts commit f264cd6415b35d0eb337d4b79be49b718dce5d5a.

--- C#/Demo/DemoApplication/Form1.cs
+++ C#/Demo/DemoApplication/Form1.cs
@@ -7,21 +7,13 @@ namespace DemoApplication
 	{
 		public Form1()
 		{
-			AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
-
 			InitializeComponent();
-
 
 			//AppDomain.CurrentDomain.ExecuteAssembly()
 
 			//AppDomainSetup.ApplicationBase = Path.Combine(AppDomainSetup.ApplicationBase, "..\\..");
 			//var myAssembly = Assembly.LoadFrom(Path.Join(appDir, "Component.dll"));
 
-		}
-
-		private Assembly? CurrentDomain_AssemblyResolve(object? sender, ResolveEventArgs args)
-		{
-			throw new NotImplementedException();
 		}
 	}
 }
