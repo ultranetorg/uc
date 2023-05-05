1c1d05d54449e6b19d8cfddba1fd3c6eb3e142bb
mooshra <31731931+mooshra@users.noreply.github.com>
2023-05-05 20:12:47 +0300

    Release Build Preparation

--- Umc/Umc/Platforms/Android/MainActivity.cs
+++ Umc/Umc/Platforms/Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Plugin.Fingerprint;
 
 namespace UC.Umc;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+[Activity(MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
 public class MainActivity : MauiAppCompatActivity
 {
     protected override void OnCreate(Bundle savedInstanceState)
