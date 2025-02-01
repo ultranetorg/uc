7e468963803f4b3a01eb610e2442aaa000970fa3
Maximion <a@ultranet.org>
2025-02-01 13:49:19 +0200

    same, improvements

--- Smp/cs/Publication/PublicationTable.cs
+++ Smp/cs/Publication/PublicationTable.cs
@@ -16,48 +16,12 @@ public class PublicationTable : Table<PublicationEntry>
 
 	public PublicationEntry Find(EntityId id, int ridmax)
 	{
-		//if(0 < ridmax && ridmax < Database.Tail.Last().Id - 1)
-		//	throw new IntegrityException("maxrid works inside pool only");
-
   		foreach(var i in Tail.Where(i => i.Id <= ridmax))
 			if(i.AffectedPublications.TryGetValue(id, out var r))
     			return r;
 
 		var e = FindBucket(id.B)?.Entries.Find(i => i.Id.E == id.E);
 
-		//if(e == null)
-		//	return null;
-
-		//e.Address.Publisher = Mcv.Publishers.Find(id, ridmax).Address;
-
 		return e;
 	}
-	
-// 	public CardEntry Find(Ura address, int ridmax)
-// 	{
-//  		//if(0 < ridmax && ridmax < Database.Tail.Last().Id - 1)
-//  		//	throw new IntegrityException("maxrid works inside pool only");
-// 
-//   		foreach(var r in Tail.Where(i => i.Id <= ridmax))
-//  		{	
-//  			var x = r.AffectedCards.Values.FirstOrDefault(i => i.Address == address);
-//  					
-//  			if(x != null)
-//   				return x;
-//  		}
-//  
-//         var d = Mcv.Publishers.Find(address.Publisher, ridmax);
-// 
-//         if(d == null)
-//             return null;
-// 
-//   		var e = FindBucket(d.Id.H)?.Entries.Find(i => i.Id.E == d.Id.E && i.Address.Card == address.Card);
-// 
-// 		if(e == null)
-// 			return null;
-// 
-// 		e.Address.Publisher = d.Address;
-// 
-// 		return e;
-// 	}
  }
