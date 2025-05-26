ca958abe639768b6c0fe3d2e7d32665c3992ee65
elwray <elwray.ru@gmail.com>
2025-05-26 22:23:35 +0200

    Fair.WebUI:
    - Sidebar and Sidebar components design.

--- Fair/WebUI/src/ui/layouts/Base/Sidebar.tsx
+++ Fair/WebUI/src/ui/layouts/Base/Sidebar.tsx
@@ -1,11 +1,10 @@
 import { memo } from "react"
 import { Link } from "react-router-dom"
+import { useTranslation } from "react-i18next"
 import { twMerge } from "tailwind-merge"
 
-import { Grid3x3GapFillSvg } from "assets"
 import { PropsWithClassName } from "types"
-import { PrimaryButton, SidebarSitesList } from "ui/components"
-import { useTranslation } from "react-i18next"
+import { SidebarButton, SidebarSitesList } from "ui/components"
 
 const TEST_CURRENT_SITE: SidebarSite = {
   id: "0",
@@ -34,11 +33,7 @@ export const Sidebar = memo(({ className }: PropsWithClassName) => {
     <div className={twMerge("min-w-65 flex flex-col gap-6 p-2", className)}>
       <div className="flex flex-col gap-8 p-2">
         <Link to="/">
-          <PrimaryButton
-            className="w-full"
-            image={<Grid3x3GapFillSvg className="fill-stone-800 stroke-stone-800" />}
-            label="All Sites"
-          />
+          <SidebarButton title={t("allSites")} />
         </Link>
         <SidebarSitesList title={t("currentSite")} items={[TEST_CURRENT_SITE]} />
         <SidebarSitesList title={t("starredSites")} items={TEST_SITES} />
