1bce36cce0e043d659e3f5b851120d3f23e34afb
elwray <elwray.ru@gmail.com>
2025-05-30 13:33:53 +0200

    Fair.WebUI: - Breadcrumbs component.

--- Fair/WebUI/src/ui/components/Breadcrumbs.tsx
+++ Fair/WebUI/src/ui/components/Breadcrumbs.tsx
@@ -2,21 +2,26 @@ import React, { memo } from "react"
 import { Link } from "react-router-dom"
 
 import { ChevronRightSvg, ThreeDotsSvg } from "assets"
+import { formatTitle } from "utils"
 
 export type BreadcrumbsItemProps = {
   title: string
   path?: string
 }
 
-const BreadcrumbsItem = memo(({ title, path }: BreadcrumbsItemProps) =>
-  path ? (
-    <Link className="text-dark-100" to={path}>
-      {title}
+const BreadcrumbsItem = memo(({ title, path }: BreadcrumbsItemProps) => {
+  const formattedTitle = formatTitle(title)
+
+  return path ? (
+    <Link className="text-dark-100" to={path} title={title}>
+      {formattedTitle}
     </Link>
   ) : (
-    title
-  ),
-)
+    <span className="text-gray-400" title={title}>
+      {formattedTitle}
+    </span>
+  )
+})
 
 export type BreadcrumbsProps = {
   items: BreadcrumbsItemProps[]
@@ -28,13 +33,13 @@ export const Breadcrumbs = memo(({ items }: BreadcrumbsProps) => {
   }
 
   return (
-    <div className="flex h-6 items-center gap-1 text-2xs leading-5">
+    <div className="flex h-6 select-none items-center gap-1 text-2xs leading-5">
       {items.length === 1 ? (
         <BreadcrumbsItem {...items[0]} />
       ) : items.length === 2 ? (
         <>
           <BreadcrumbsItem {...items[0]} />
-          <ChevronRightSvg />
+          <ChevronRightSvg className="stroke-gray-400" />
           <BreadcrumbsItem {...items[1]} />
         </>
       ) : (
@@ -43,14 +48,14 @@ export const Breadcrumbs = memo(({ items }: BreadcrumbsProps) => {
             <BreadcrumbsItem key={i} {...x} />
           ) : i === 1 ? (
             <React.Fragment key={i}>
-              <ChevronRightSvg />
-              <ThreeDotsSvg />
-              <ChevronRightSvg />
+              <ChevronRightSvg className="stroke-gray-400" />
+              <ThreeDotsSvg className="fill-gray-400" />
+              <ChevronRightSvg className="stroke-gray-400" />
               <BreadcrumbsItem {...x} />
             </React.Fragment>
           ) : (
             <React.Fragment key={i}>
-              <ChevronRightSvg />
+              <ChevronRightSvg className="stroke-gray-400" />
               <BreadcrumbsItem {...x} />
             </React.Fragment>
           ),
