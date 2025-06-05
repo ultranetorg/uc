64fbd4aef07a8ea17184758f88a1995d4af4f9d1
elwray <elwray.ru@gmail.com>
2025-06-05 17:11:59 +0200

    Fair.WebUI:
    - DisputeComments loading added.

---
+++ Fair/cs/WebServer/Controllers/Moderator/ModeratorDisputeCommentsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Uccs.Web.Pagination;
+
+namespace Uccs.Fair;
+
+[Route("api/moderator/sites/{siteId}/disputes/{disputeId}")]
+public class ModeratorDisputeCommentsController
+(
+	ILogger<ModeratorDisputeCommentsController> logger,
+	IAutoIdValidator autoIdValidator,
+	IPaginationValidator paginationValidator,
+	IDisputeCommentsService disputeCommentsService
+) : BaseController
+{
+	[HttpGet]
+	public IEnumerable<DisputeCommentModel> GetDisputeComments(string disputeId, [FromQuery] PaginationRequest pagination, CancellationToken cancellationToken)
+	{
+		logger.LogInformation($"GET {nameof(ModeratorDisputeCommentsController)}.{nameof(ModeratorDisputeCommentsController.GetDisputeComments)} method called with {{DisputeId}}, {{Pagination}}", disputeId, pagination);
+
+		autoIdValidator.Validate(disputeId, nameof(Dispute).ToLower());
+		paginationValidator.Validate(pagination);
+
+		(int page, int pageSize) = PaginationUtils.GetPaginationParams(pagination);
+		TotalItemsResult<DisputeCommentModel> reviews = disputeCommentsService.GetDisputeComments(disputeId, page, pageSize, cancellationToken);
+
+		return this.OkPaged(reviews.Items, page, pageSize, reviews.TotalItems);
+	}
+}
---
+++ Fair/cs/WebServer/Services/IDisputeCommentsService.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Uccs.Web.Pagination;
+
+namespace Uccs.Fair;
+
+public interface IDisputeCommentsService
+{
+	TotalItemsResult<DisputeCommentModel> GetDisputeComments([NotNull][NotEmpty] string disputeId, [NonNegativeValue] int page,
+		[NonNegativeValue][NonZeroValue] int pageSize, CancellationToken cancellationToken);
+}
