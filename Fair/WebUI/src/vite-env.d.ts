1f64e4e48d8939c3a5a19fe9c77e788b9cf4ee60
elwray <elwray.ru@gmail.com>
2025-06-26 21:49:43 +0200

    Fair.WebUI:
    - Switched from yarn to npm.
    - Fixed Swiper compilation errors.

--- Fair/WebUI/src/vite-env.d.ts
+++ Fair/WebUI/src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+
+declare module "swiper/css"
+declare module "swiper/css/navigation"
+declare module "swiper/css/pagination"
+declare module "swiper/css/effect-fade"
