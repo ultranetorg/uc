ae639f138711298b0b5eb3b4a9cc3a027430582e
elwray <elwray.ru@gmail.com>
2025-07-14 15:26:04 +0200

    Fair.WebUI:
    - Table component improvements.
    - ReferendumsPage fix and improvements.

--- Fair/WebUI/src/ui/components/Table/TableBody.tsx
+++ Fair/WebUI/src/ui/components/Table/TableBody.tsx
@@ -5,23 +5,26 @@ import { twMerge } from "tailwind-merge"
 import { TableProps } from "./types"
 import { TableRow } from "./TableRow"
 
-export type TableBodyProps = Pick<TableProps, "columns" | "emptyState" | "items" | "itemRenderer">
+export type TableBodyProps = Pick<TableProps, "columns" | "emptyState" | "items" | "itemRenderer" | "rowRenderer">
 
-export const TableBody = memo(({ columns, emptyState, items, itemRenderer }: TableBodyProps) =>
+export const TableBody = memo(({ columns, emptyState, items, itemRenderer, rowRenderer }: TableBodyProps) =>
   items && items.length > 0 ? (
-    items.map(item => (
-      <TableRow key={item.id}>
-        {columns.map(column => {
-          const value = get(item, column.accessor)
-          const renderedValue = itemRenderer && itemRenderer(item, column)
-          return (
-            <div className={column.className ?? "flex-1"} key={`${item.id}_${column.accessor}`}>
-              {renderedValue ?? value?.toString?.() ?? ""}
-            </div>
-          )
-        })}
-      </TableRow>
-    ))
+    items.map(item => {
+      const content = (
+        <TableRow key={item.id}>
+          {columns.map(column => {
+            const value = get(item, column.accessor)
+            const renderedValue = itemRenderer && itemRenderer(item, column)
+            return (
+              <div className={column.className ?? "flex-1"} key={`${item.id}_${column.accessor}`}>
+                {renderedValue ?? value?.toString?.() ?? ""}
+              </div>
+            )
+          })}
+        </TableRow>
+      )
+      return rowRenderer ? rowRenderer(content, item) : content
+    })
   ) : (
     <TableRow className={twMerge(emptyState && "py-6")}>{emptyState ? emptyState : null}</TableRow>
   ),
