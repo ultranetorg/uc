7eae18f04a7e3677bc26c335ff8935726a43ca39
Azat Buriev <adwaer@live.ru>
2025-11-12 22:03:26 +0300

    Lint config

--- Fair/WebUI/eslint.config.js
+++ Fair/WebUI/eslint.config.js
@@ -1,28 +1,37 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import tseslint from "typescript-eslint"
+import eslintPluginPrettier from "eslint-plugin-prettier"
+import tailwindcss from "eslint-plugin-tailwindcss"
+import eslintConfigPrettier from "eslint-config-prettier"
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      tailwindcss,
+      prettier: eslintPluginPrettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      // prettier
+      ...eslintConfigPrettier.rules,
+      ...eslintPluginPrettier.configs.recommended.rules,
+      // tailwind
+      ...tailwindcss.configs.recommended.rules,
+      // ensure the prettier rule is enabled
+      "prettier/prettier": "error",
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     },
   },
 )
