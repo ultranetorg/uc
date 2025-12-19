1da0e952de567e041fe6263eaeb6f9c4323e9ac3
elwray <elwray.ru@gmail.com>
2025-12-19 14:37:42 +0100

    Fair.WebUI:
    - Small cleanup.

--- Fair/WebUI/src/ui/components/specific/PublicationStoresModal/PublicationStoreRow.tsx
+++ Fair/WebUI/src/ui/components/specific/PublicationStoresModal/PublicationStoreRow.tsx
@@ -8,8 +8,8 @@ export type PublicationStoreRowProps = {
 
 export const PublicationStoreRow = ({ title, publicationDate }: PublicationStoreRowProps) => (
   <div className="flex cursor-pointer items-center gap-3 p-2 text-2sm leading-5">
-    <div className="h-10 w-10 overflow-hidden rounded-lg">
-      <img src={PUBLICATIONS_STORE_ROW_TEST_IMAGE} className="h-full w-full object-cover" />
+    <div className="size-10 overflow-hidden rounded-lg">
+      <img src={PUBLICATIONS_STORE_ROW_TEST_IMAGE} className="size-full object-cover" />
     </div>
     <span className="w-[45%] overflow-hidden text-ellipsis text-nowrap font-medium">{title}</span>
     <span className="w-[30%]">{publicationDate}</span>
