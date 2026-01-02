980649301df09edc0f49919930b2e025359d3574
belosuo <privacynoter@proton.me>
2026-01-03 00:00:09 +0700

    modal fix

--- Fair/WebUI/src/ui/components/TextModal.tsx
+++ Fair/WebUI/src/ui/components/TextModal.tsx
@@ -15,15 +15,18 @@ export type TextModalProps = Pick<ModalProps, "title"> & TextModalBaseProps
 
 export const TextModal = memo(({ title, text, onConfirm, onCancel, confirmLabel, cancelLabel }: TextModalProps) => {
   const handleClose = onCancel ?? onConfirm
+  const hasCancel = !!onCancel && !!cancelLabel
 
   useEscapeKey(handleClose)
 
   return (
     <Modal className="w-135 gap-4 p-6" title={title} onClose={handleClose}>
-      <span className="text-sm">{text}</span>
-      <div className="flex items-center justify-end gap-4">
+      <div className="max-h-[60vh] overflow-y-auto text-sm pr-1">
+        {text}
+      </div>
+      <div className={`${hasCancel ? "" : "mt-3"} flex items-center justify-end gap-4`}>
         <ButtonPrimary className="h-9 w-20 capitalize" label={confirmLabel} onClick={onConfirm} />
-        {onCancel && cancelLabel && (
+        {hasCancel && (
           <ButtonOutline className="h-9 w-20 capitalize" label={cancelLabel} onClick={onCancel} />
         )}
       </div>
